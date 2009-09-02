36af3eef350fe524ce59bb5c9d917041f6090e97
Ignacio Trujillo <+@ignac.es>
2009-09-02 17:35:58 -0400

    version 0.9

---
+++ include/datos/historia.php
@@ -0,0 +1,31 @@
+<!-- articulo: acerca -->
+<div id="articulo">
+	<div id="titulodelarticulo">
+  <img style="position:absolute;" src="static/img/acerca/acerca.png" />
+        <img style="float:right; padding-right:20px; position:static;" src="static/img/acerca/irfe_e.png"/>
+	</div>
+    
+
+<p id="banner"><img src="static/img/home_buttom.png" /> <a style="color:#333333;" href="htpp://irfe.cl">Portada</a> » Acerca del IRFE </p> 
+
+<div id="texto">
+<p>Colegio fundado en 1940, por la Fundación Centro Cristiano, entidad que tiene entre sus fines promover y fomentar la Educación Católica de la Juventud. La Fundación entrega a los Padres Pallottinos Schoenstattianos la responsabilidad  de construir y administrar esta casa de estudios, siendo su primer Rector el Padre José Kuhl.</p>
+<p>En 1948 visita el colegio el Padre José Kentenich quien coloca la primera piedra del Santuario de Schoenstatt en Santa Cruz, su homilía, en esa ceremonia, es un verdadero legado inspirador del Proyecto Educativo del Colegio.</p>
+<br />
+<p>El Instituto Regional Federico Errázuriz tiene sus raíces en el espíritu de los Padres fundadores que venidos de la ciudad de Limburgo en Alemania, imprimieron su impronta pedagógica y d excelencia que se mantiene y se mantendrá en el quehacer de esta prestigiosa Institución.</p>
+<br />
+<ul>Se mantiene desde el origen como un verdadero estilo Institutano, entre otras las siguientes características:</ul> 
+<br />
+<li>Un edificio de gran belleza arquitectónica.</li>
+<li>Enseñanza que conjuga alta exigencia Académica y sólida Formación Cristiana.</li>
+<li>Colores institucionales el rojo, negro y amarillo que están en el edificio, estandarte, bandera, insignia y otros símbolos.</li>
+<li>Himno del colegio creado por el Padre José Kulh.</li>
+<li>Banda de guerra que acompaña los desfiles institucionales.</li>
+<li>Sólidos resultados académicos y proyección de sus alumnos a Educación Universitaria.</li>
+<br />
+<p>En 1968 los Padres Pallotlinos se retiran de Chile y el colegio vuelve a la Fundación Centro Cristiano que lo administra en la actualidad. Imparte Educación Pre-Básica, Básica y Enseñanza Media con una matricula de 1.753 alumnos. Una de sus fortalezas es el fomento del uso del Tiempo Libre y el desarrollo de los intereses de los alumnos para lo cual ofrece 37 acles en la áreas de deporte, artística, científico-tecnológica y cívico-social.</p>
+<br />
+<p>El lema del colegio es: <strong>“Dios y Patria”</strong>.</p>
+</div>
+</div>
+<!-- /articulo -->
--- index.php
+++ index.php
@@ -2,117 +2,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml"> 
 
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="device-width" content="1280" user-scalable="no"/> 
-    <meta name="viewport" content="width=1180" user-scalable="no"/>   
-    <meta name="apple-mobile-web-app-capable" content="yes"/>
-    
-    <title>IRFE.cl | Portada</title>
-    <meta name="Keywords" content="IRFE, instituto, regional, federico, errazuriz, santa, cruz, chile, colegio, instituto, catolico, centro, cristiano, ignaces, ignaciotrujillo" /> 
-    <meta name="Description" content="Sitio oficial del prestigioso Instituto Regional Federico Errázuriz, ubicado en Santa Cruz, Chile. Nuetsra meta principal es formar personas de bien, con bastos conocimientos, preparados para enfrentar los estudios superiores y fundamentalmente, basar su fe en Dios." />
-    <meta name="Author" content="Ignacio Trujillo Rojas ; ignacio[at]dr[dot]com" />
-    
-    <link rel="shortcut icon" href="http://static.irfe.cl/favicon.ico" /> 
-    
-    <script src="static/js/twitter.min.js" type="text/javascript"></script>  
-    <script src="static/js/reflection.js" type="text/javascript"></script>
-    <script src="static/js/getwitter.js" type="text/javascript"></script>
-    <script src="static/js/jquery.min.js"></script> 
-    <script src="static/js/presentador.js"></script>
-    <!--[if lte IE 8]>
-    <style media="all" type="text/css">@import "static/css/explorer.css";</style>
-    <![endif]-->
-    <style media="all" type="text/css">@import "static/css/navbar.css";</style>
-    <style media="all" type="text/css">@import "static/css/html_body.css";</style>
-    <style media="all" type="text/css">@import "static/css/tweetbox.css";</style>
-    <style media="all" type="text/css">@import "static/css/presentador_html5.css";</style>
-    <style media="all" type="text/css">@import "static/css/footer.css";</style>
-    <style media="all" type="text/css">@import "static/css/char.css";</style>
-    <style media="all" type="text/css">@import "static/css/webkit_animacion.css";</style>
-        
+<title>IRFE.cl | Portada</title>
+<meta name="Keywords" content="IRFE, instituto, regional, federico, errazuriz, santa, cruz, chile, colegio, instituto, catolico, centro, cristiano, ignaces, ignaciotrujillo" /> 
+<meta name="Description" content="Sitio oficial del prestigioso Instituto Regional Federico Errázuriz, ubicado en Santa Cruz, Chile. Nuetsra meta principal es formar personas de bien, con bastos conocimientos, preparados para enfrentar los estudios superiores y fundamentalmente, basar su fe en Dios." />
+<?php
+include 'include/head.php';?>
+<?php
+include 'include/import/index.php';?>
 </head> 
 
 <body>
-<!-- barradenavegacion -->
-<div id="navbar">
-		<ul id="navbarul"> 
-			<li id="navbarli">
-            	<a href="">Portada</a>
-            </li>
-			<li id="navbarli">
-            	<a href="http://irfe.cl/historia" title="Historia del IRFE">Historia</a>
-            </li> 
-			<li id="navbarli">
-            	<a href="http://noticias.irfe.cl">Noticias</a>
-            </li>
-            <li id="navbarli">
-            	<a href="http://irfe.cl/calendario">Calendario</a>
-            </li>
-			<li id="navbarli">
-            	<a href="http://irfe.cl/calendario">Alumnado</a>
-            </li> 
-            <li id="navbarli">
-            	<a href="http://twitter.com/irfe" title="S&iacute;guenos en Twitter">
-                	<img style="margin-top:-2px; border:0px;" src="static/img/index1/twitter.png"/>
-                </a>
-            </li>
-            <li id="navbarli">
-            	<a href="http://facebook.com/irfesantacruz" title="Hazte fan de nosotros en Facebook">
-            		<img style="margin-top:-2px; border:0px" src="static/img/index1/facebook.png" />
-                </a>
-             </li>
-            <li id="navbarli">
-            	<a href="http://youtube.com/irfesantacruz" title="Ve nuestros videos en Youtube">
-            		<img style="margin-top:-2px; border:0px" src="static/img/index1/youtube.png" />
-                </a>
-            </li>
-		</ul>
-</div>
-<!-- /barradenavegacion -->
-
-<!-- presentador -->
-<div id="wraper">
-    <div id="presentador"> 
-        <ul class="slides"> 
-            <li>
-                <img src="static/img/index1/photos/2/1.jpg" width="1080" height="496">
-                    <div id="inputpresentacion">
-                        <p id="contenidoinput">D&iacute;a Del Alumno 2011</p>
-                    </div>
-                </img>
-            </li>
-            <li><img src="static/img/index1/transparent.png" width="1080" height="496"/>
-            </li> 
-            <li><img src="static/img/index1/photos/3.jpg" width="1080" height="496"/>
-            </li> 
-            <li><img src="static/img/index1/photos/5.png" width="1080" height="496"/>
-            </li> 
-        </ul> 
-        <span class="arrow previous"></span> 
-        <span class="arrow next"></span> 
-    </div>
-</div>
-<!-- /presentador -->
-
-<!-- ultimotweet -->
-<div id="tweetbox">
-	<div id="tweet">
-    </div>
-</div>
-<!-- /ultimotweet -->
-
-<!-- footer -->
-<div id="barrafooter">
-	<div id="infocolegio">
-		<p>Avenida Errázuriz 670 - Santa Cruz de Chile <em>|</em> <a href="mailto:irfe@irfe.cl">irfe@irfe.cl</a> <em>|</em> Tel: (72)-822253 y (72)-821731<p>
-	</div>
-
-	<div id="footer">
-		<p>Copyright <strong>©</strong> 2011 Instituto Regional Federico Errázuriz. Derechos Reservados.</p>
-		<p>Diseñado por <a href="http://twitter.com/ignaces">Ignacio Trujillo Rojas</a>. v.0.99 β</p>
-	</div>
-</div>
-<!-- /footer -->
- 
+<?php
+include 'include/navbar.php';?>
+<?php
+include 'include/presentador.php';?>
+<?php
+include 'include/tweet.php';?>
+<?php
+include 'include/footer.php';?>
 </body> 
 </html> 
